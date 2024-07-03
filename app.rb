623022a42c588b2b0953545e3e6953edc203fe76
Tulio Salvatierra <salvacorp@gmail.com>
2024-07-03 11:23:23 +0000

    chore: Add routes for rock, paper, and scissors games

--- app.rb
+++ app.rb
@@ -7,3 +7,33 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/rock") do
+
+  moves = ["rock", "paper", "scissors"]
+  
+  @computer_move = moves.sample
+
+  if @computer_move == "rock"
+    @outcome = "We tied!"
+  elsif @computer_move == "paper"
+    @outcome = "They won!"
+  else
+    @outcome = "We won!"
+  end
+erb (:rock )
+end
+
+get("/paper") do
+  "
+  <h1>Welcome to your Sinatra App!</h1>
+  <p>Define some routes in app.rb</p>
+  "
+end
+
+get("/scissors") do
+  "
+  <h1>Welcome to your Sinatra App!</h1>
+  <p>Define some routes in app.rb</p>
+  "
+end
